795798b15e875a0cef15b6ac43255a96343dd843
thaytuh <htaylorkrass@gmail.com>
2025-07-14 10:23:05 -0700

    initial project setup

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=sys.argv[1]
+)
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
